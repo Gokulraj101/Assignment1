871e86d8bbb62d6a19ae65d8edbf407bcf6fdec2
Gokulraj N <gokulraj.n@prodapt.com>
2022-07-18 16:51:38 +0530

    source code

---
+++ SimpleIntrest.java
@@ -0,0 +1,13 @@
+package com.Prodapt.OOPbasics;
+
+public class SimpleIntrest {
+	static int p=2000,r=4,t=2,amount,SI;
+	static void si()
+	{
+		SI=(p*r*t)/100;
+		amount=SI+p;
+		System.out.println("The simpleintrest is "+SI);
+		amount=SI+p;
+		System.out.println("Total amount is "+amount);
+	}
+}
